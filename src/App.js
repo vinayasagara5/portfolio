2dbf6a5c466b48f2b560ca41957e2ee2ffa1f0ec
vinayasaga <sridhar@sridhars-MacBook-Air.local>
2024-05-22 13:42:36 +0530

    added shadow on curser

--- src/App.js
+++ src/App.js
@@ -1,33 +1,30 @@
-
 import { useState } from 'react';
-import './App.css';
-import style from './App.module.css'
-import Main from './Main';
 import PortPolfolio from './PortPolfolio';
+import './App.css';
 
 
 function App() {
-  // const [isHovered, setIsHovered] = useState(false);
-  // const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
+  const [isHovered, setIsHovered] = useState(false);
+  const [cursorPosition, setCursorPosition] = useState({ x: 0, y: 0 });
 
-  // const handleMouseMove = (e) => {
-  //   setCursorPosition({ x: e.clientX, y: e.clientY });
-  // };
+  const handleMouseMove = (e) => {
+    setCursorPosition({ x: e.clientX, y: e.clientY });
+  };
+
   return (
-    <div className={style.app}
-    // onMouseMove={handleMouseMove}
-    //   // className="hover-area"
-    //   onMouseEnter={() => setIsHovered(true)}
-    //   onMouseLeave={() => setIsHovered(false)}
+    <div className="App"
+      onMouseMove={handleMouseMove}
+      onMouseEnter={() => setIsHovered(true)}
+      onMouseLeave={() => setIsHovered(false)}
     >
-      {/* {isHovered && (
+      {isHovered && (
         <div
           className="shadow"
-          style={{ top: cursorPosition.y, left: cursorPosition.x }}
+          style={{
+            background: `radial-gradient(600px at ${cursorPosition.x}px ${cursorPosition.y}px, rgba(29, 78, 216, 0.15), transparent 80%)`
+          }}
         />
-      )} */}
-     
-
+      )}
       <PortPolfolio />
     </div>
   );
